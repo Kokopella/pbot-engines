bc8f6c98ee9fb3b4ad2444ce46936390c028271b
Kokopella <koko.puella@gmail.com>
2020-06-23 19:32:15 -0400

    Initial commit

---
+++ tsumokiri.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+module Tsumokiri
+
+  # Required
+  # Called whenever a tile is dealt to Pbot
+  def makeDealDecision(operations, tile)
+    hand = @round[@seat][:hand]
+    keys = operations.keys
+    discard = tile
+    type = 1
+    index = nil
+
+    # Tsumo
+    if keys.include?(8)
+      # Do tsumo things
+
+    # Riichi
+    elsif keys.include?(7)
+      # Do riichi things
+
+    # Discard
+    else
+      discard = chooseDiscard(hand + tile)
+    end
+
+    # Sets moqie value, updates Pbot's hand, then sends the action message
+    # I would not recommend changing these
+    moqie = isMoqie(discard)
+    updateHand(tile, discard)
+    doInputOperation(type: type, index: index, tile: discard, moqie: moqie)
+  end
+
+  # Required
+  # Called whenever another player discards a tile
+  def makeDiscardDecision(operations, tile)
+    hand = @round[@seat][:hand]
+    keys = operations.keys
+    discard = tile
+    type = nil
+    index = nil
+    cancelOp = 1
+
+    if keys.include?(9)
+      # Do Ron things
+
+    else
+      # Or just ignore every call
+    end
+
+    # Send response
+    # Don't change this
+    doInputChiPengGang(type: type, index: index, cancelOp: cancelOp)
+  end
+
+  # Optional
+  # You choose what tile to discard however you damn please
+  def chooseDiscard(hand)
+    hand.pop
+  end
+end
